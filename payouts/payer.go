e0b2c3067c92cb83335c1d2c3651a5eee6e90002
vbank <36046649+vbank@users.noreply.github.com>
2018-07-18 17:22:05 +0300

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -10,9 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/techievee/open-ethereum-pool/rpc"
-	"github.com/techievee/open-ethereum-pool/storage"
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/vbank/open-ethereum-pool-1/rpc"
+	"github.com/vbank/open-ethereum-pool-1/storage"
+	"github.com/vbank/open-ethereum-pool-1/util"
 )
 
 const txCheckInterval = 5 * time.Second

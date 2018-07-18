6152c6e4c5f2d504b85cf3ff6f6a73dd8a6b4e2f
vbank <36046649+vbank@users.noreply.github.com>
2018-07-18 17:19:20 +0300

    Update blocks.go

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/techievee/open-ethereum-pool/rpc"
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/vbank/open-ethereum-pool-1/rpc"
+	"github.com/vbank/open-ethereum-pool-1/util"
 )
 
 const maxBacklog = 3
